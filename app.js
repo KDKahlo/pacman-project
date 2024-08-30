bda69bf4eef8ffe6896a8d940524e76bcddae81c
KD <kdkahlo@gmail.com>
2024-08-30 14:55:23 +0200

    pac-man game files initiated. createBoard() complete and some styling

---
+++ app.js
@@ -0,0 +1,92 @@
+//Imagine you’re setting up a game, but you want to make sure everything is ready before you start. 
+//This line is like a signal that says, "Hey, don't start until all the pieces of the game board are in place." 
+//It waits until the basic structure of your webpage (like the grid where Pac-man will move) 
+//is fully set up before running the code inside it.
+
+//we want to make sure we listen for all elements to load from the index.html so we use an event listener for 'DOMContentLoad'
+document.addEventListener('DOMContentLoaded', () => {
+    //This line is like saying, "Where’s the scoreboard?" 
+    //It finds the part of the webpage where the score will be shown. 
+    //It’s like pointing to a specific spot on the screen and saying, 
+    //"This is where we’ll keep track of the score."
+
+    // allows us to update the score that is displayed to the user. 
+    //the DOM syntax selects are HTML elements with id 'score' & assigns them to 'scoreDisplay' variable
+    const scoreDisplay = document.getElementById('score')
+    //square for the gameboard grid
+    const width = 28
+    //the score should start at zero and increase as the game is played
+    let score = 0
+
+   //This line is like saying, "Where’s the game board?" 
+   //It finds the main area on the screen where the Pac-man game will happen.
+    const grid = document.querySelector('.grid')
+
+    const layout = [
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1,
+        1, 3, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 3, 1,
+        1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1,
+        1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+        1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 1, 1, 1, 2, 2, 1, 1, 1, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 1, 2, 2, 2, 2, 2, 2, 1, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+        4, 4, 4, 4, 4, 4, 0, 0, 0, 4, 1, 2, 2, 2, 2, 2, 2, 1, 4, 0, 0, 0, 4, 4, 4, 4, 4, 4,
+        1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 1, 2, 2, 2, 2, 2, 2, 1, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+        1, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1,
+        1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1,
+        1, 3, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 3, 1,
+        1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1,
+        1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1,
+        1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1,
+        1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+    ]
+    //to create the gameboard grid, we will use an array. The edge of the grid will have 28 '1s' for the 
+    // 0 - pac-dots
+    // 1 - wall
+    // 2 - ghost-lair
+    //3 - power-pellet
+    // 4 empty
+    const squares = []
+
+    //create the gameboard
+    function createBoard() {
+        for (let i = 0; i < layout.length; i++) {
+            //Imagine you need to place a small square on the game board. 
+            //This line is like cutting out a new square piece of paper, but you’re not putting it on the board just yet.
+            const square = document.createElement('div')   
+            //Now that you have your square piece of paper, this line is like gluing it onto the game board.
+            // It makes the square actually appear on the screen where the game will be played.     
+            grid.appendChild(square)
+            squares.push(square)
+
+            //add layout to the board
+            //we now need to add class to each of the div's created by the createBoard(). 
+            //to do this we use the if statement to define what the classname will be for each index value in the array.
+            if (layout[i] === 0) {
+                squares[i].classList.add('pac-dot')                                
+            }
+        }   
+    }
+    //call the function to create the gameboard
+    createBoard()
+
+
+
+
+
+
+
+    
+})
